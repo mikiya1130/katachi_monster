e8827cd2a28ece843077bb8f91d058b85c5f9674
Morisaki Mikiya <git@morisakimikiya.com>
2023-10-12 08:08:46 +0900

    Revert "test"
    This reverts commit 33e517f3512f6bcba44e7549a18696f7098c989c.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Button from "@mui/material/Button";
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <Button variant="contained">Hello World</Button>
     </div>
   );
-}
+};
+
+export default Home;
