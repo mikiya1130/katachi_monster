1af99c11cc26ba7bff3394e4a49728d3cab7d6fe
Morisaki Mikiya <git@morisakimikiya.com>
2023-10-12 08:17:25 +0900

    Revert "test: format"
    This reverts commit 6f03b9ae1dbf6e58f141949c5fc7b8fcd496f7df.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 const Home = () => {
   return (
     <div>
-        <Button variant="contained">Hello World</Button>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 };
