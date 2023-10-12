162636864490621f7153f8c670f435c63b176790
Morisaki Mikiya <git@morisakimikiya.com>
2023-10-12 19:43:20 +0900

    update: template

---
+++ frontend/src/app/theme.ts
@@ -0,0 +1,13 @@
+// 参考：Default theme viewer (https://mui.com/material-ui/customization/default-theme/)
+
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default theme;
