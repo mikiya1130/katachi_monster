8688e42a7f1f1d89af5acb0be60b956c4c117d8b
Morisaki Mikiya <git@morisakimikiya.com>
2023-12-10 08:10:43 +0900

    update: error handring of read db

--- backend/src/cruds/picture.py
+++ backend/src/cruds/picture.py
@@ -1,4 +1,6 @@
 """pictures テーブルの CRUD 関数"""
+from fastapi import HTTPException
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 from src.models import Picture
@@ -13,4 +15,12 @@ def create_picture(db: Session, db_picture: Picture) -> Picture:
 
 def read_picture(db: Session, picture_id: int) -> Picture:
     """pictures テーブルの指定された id のレコードを取得する"""
-    return db.query(Picture).filter(Picture.id == picture_id).first()  # type: ignore
+    db_picture = (
+        db.query(Picture)
+        .filter(Picture.id == picture_id)
+        .order_by(desc(Picture.id))
+        .first()
+    )
+    if db_picture is None:
+        raise HTTPException(status_code=500, detail="Picture not found")
+    return db_picture
--- backend/src/cruds/silhouette.py
+++ backend/src/cruds/silhouette.py
@@ -1,4 +1,5 @@
 """silhouettes テーブルの CRUD 関数"""
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from src.models import Silhouette
@@ -6,8 +7,7 @@ from src.models import Silhouette
 
 def read_silhouette(db: Session, silhouette_id: int) -> Silhouette:
     """silhouettes テーブルの指定された id のレコードを取得する"""
-    return (
-        db.query(Silhouette)  # type: ignore
-        .filter(Silhouette.id == silhouette_id)
-        .first()
-    )
+    db_silhouette = db.query(Silhouette).filter(Silhouette.id == silhouette_id).first()
+    if db_silhouette is None:
+        raise HTTPException(status_code=500, detail="Silhouette not found")
+    return db_silhouette
