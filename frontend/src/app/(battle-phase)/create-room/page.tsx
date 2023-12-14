119377a8a5c05a22d93caffa53a9206345c94a14
Morisaki Mikiya <git@morisakimikiya.com>
2023-12-15 06:50:43 +0900

    test

--- frontend/src/app/(battle-phase)/create-room/page.tsx
+++ frontend/src/app/(battle-phase)/create-room/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 import { Stack } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { io } from "socket.io-client";
 
 const CreateRoom = () => {
   const title = "へやをつくる";
   const ID = 1234;
+
+  const handleConnect = () => {
+    console.log("clicked");
+    const socket = io("http://localhost:3333");
+    socket.on("connect", () => {
+      console.log("client: connected");
+    });
+  };
 
   return (
     <Stack
@@ -13,6 +22,7 @@ const CreateRoom = () => {
       alignItems="center"
       justifyContent="center"
       height="100%"
+      onClick={handleConnect}
     >
       <Typography fontSize="2rem" align="left">
         {title}
---
+++ websocket/websocket.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server, { cors: { origin: "https://localhost:3000" } });
+
+const PORT = 3333;
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("server: connected");
+});
