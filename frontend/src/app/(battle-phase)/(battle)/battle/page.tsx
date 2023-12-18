7b876081e057feb27e820c9bbfb038938a231798
Morisaki Mikiya <git@morisakimikiya.com>
2023-12-18 18:51:14 +0900

    update: バトル画面 エラー処理

--- frontend/src/app/(battle-phase)/(battle)/battle/page.tsx
+++ frontend/src/app/(battle-phase)/(battle)/battle/page.tsx
@@ -16,6 +16,7 @@ import { BattleContext } from "@/app/(battle-phase)/(battle)/layout";
 import { axios } from "@/axios";
 import Centering from "@/components/Centering";
 import Image from "@/components/Image";
+import Message, { MessageRef } from "@/components/Message";
 import { useSocket } from "@/components/SocketProvider";
 import { images } from "@/consts";
 import { TypeHand } from "@/types";
@@ -24,6 +25,7 @@ const BattleAttackSelect = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const socket = useSocket();
+  const messageRef = useRef<MessageRef>(null);
 
   const [state, setState] = useState<State>("matching");
 
@@ -38,6 +40,7 @@ const BattleAttackSelect = () => {
   const [selfHand, setSelfHand] = useState<TypeHand>("gu");
   const [opponentHand, setOpponentHand] = useState<TypeHand>("gu");
   const [outcome, setOutcome] = useState<TypeOutcome>("win");
+  const [isComplete, setIsComplete] = useState<boolean>(false);
 
   const { setWinner } = useContext(BattleContext);
 
@@ -65,6 +68,26 @@ const BattleAttackSelect = () => {
       });
     });
   }, [searchParams]);
+
+  useEffect(() => {
+    const error = async () => {
+      console.error("battle-interrupt");
+      messageRef.current?.call({
+        type: "error",
+        message: "つうしんエラーがはっせいしました",
+      });
+      await sleep(3000);
+    };
+    if (socket) {
+      socket.on("battle-interrupt", () => {
+        if (!isComplete) {
+          error().then(() => {
+            router.push("/mode-select");
+          });
+        }
+      });
+    }
+  }, [isComplete, socket]);
 
   useEffect(() => {
     if (state === "matching") {
@@ -100,6 +123,15 @@ const BattleAttackSelect = () => {
       const opponentResult =
         results[Object.keys(results).find((id) => id !== socket.id) ?? ""];
 
+      const isComplete = selfResult.hp <= 0 || opponentResult.hp <= 0;
+      setIsComplete(isComplete);
+      if (isComplete) {
+        setWinner({
+          isSelf: selfResult.hp > 0,
+          monster: selfResult.hp > 0 ? monsterSelf : monsterOpponent,
+        });
+      }
+
       setSelfHand(selfResult.hand);
       setOpponentHand(opponentResult.hand);
       setState("attack/viewHand");
@@ -121,14 +153,11 @@ const BattleAttackSelect = () => {
       });
       await sleep(3000);
 
-      if (selfResult.hp > 0 && opponentResult.hp > 0) {
-        setState("buttonSelect");
-      } else {
-        setWinner({
-          isSelf: selfResult.hp > 0,
-          monster: selfResult.hp > 0 ? monsterSelf : monsterOpponent,
-        });
+      if (isComplete) {
+        socket?.disconnect();
         router.push("/battle-result");
+      } else {
+        setState("buttonSelect");
       }
     });
 
@@ -136,73 +165,76 @@ const BattleAttackSelect = () => {
   };
 
   return (
-    <Stack
-      p={4}
-      spacing={0}
-      alignItems="center"
-      justifyContent="center"
-      height="100%"
-    >
-      <Field
-        height="30%"
-        color="blue"
-        monster={monsterOpponent}
-        isSelf={false}
-      />
-
-      <Box sx={{ height: "30%", width: "100%" }}>
-        {state === "matching" && <Center>VS</Center>}
-        {state === "start" && <Center>Battle start!!</Center>}
-        {state === "buttonSelect" && <Center>Choose a button!</Center>}
-        {state === "hpCalculate" && (
-          <Center>
-            Rock! <br /> Scissors! <br /> Paper!
-          </Center>
-        )}
-        {state === "attack/viewHand" && (
-          <Box p={2} height="100%">
-            <Centering position="relative">
-              <Image
-                position="absolute"
-                top={0}
-                src={`images/${opponentHand}.png`}
-                alt={opponentHand}
-                objectFit="contain"
-                sx={{ borderRadius: "50%", height: "40%" }}
-              />
-              <Image
-                position="absolute"
-                bottom={0}
-                src={`images/${selfHand}.png`}
-                alt={selfHand}
-                objectFit="contain"
-                sx={{ borderRadius: "50%", height: "40%" }}
-              />
-            </Centering>
-          </Box>
-        )}
-        {state === "attack/viewText" &&
-          (outcome === "win" ? (
-            <Center color="red">Attack successful!</Center>
-          ) : outcome === "lose" ? (
-            <Center color="blue">Attack failed!</Center>
-          ) : (
-            outcome === "draw" && (
-              <Center color="black">It&lsquo;s a draw</Center>
-            )
-          ))}
-      </Box>
-
-      <Field height="30%" color="red" monster={monsterSelf} isSelf={true} />
-
-      <Box ref={gtpRef} sx={{ height: "10%", width: "100%" }} pt="5px">
-        <GtpButton
-          gtpHeight={gtpHeight}
-          state={state}
-          callbackButtonSelected={handleButtonSelected}
+    <>
+      <Stack
+        p={4}
+        spacing={0}
+        alignItems="center"
+        justifyContent="center"
+        height="100%"
+      >
+        <Field
+          height="30%"
+          color="blue"
+          monster={monsterOpponent}
+          isSelf={false}
         />
-      </Box>
-    </Stack>
+
+        <Box sx={{ height: "30%", width: "100%" }}>
+          {state === "matching" && <Center>VS</Center>}
+          {state === "start" && <Center>Battle start!!</Center>}
+          {state === "buttonSelect" && <Center>Choose a button!</Center>}
+          {state === "hpCalculate" && (
+            <Center>
+              Rock! <br /> Scissors! <br /> Paper!
+            </Center>
+          )}
+          {state === "attack/viewHand" && (
+            <Box p={2} height="100%">
+              <Centering position="relative">
+                <Image
+                  position="absolute"
+                  top={0}
+                  src={`images/${opponentHand}.png`}
+                  alt={opponentHand}
+                  objectFit="contain"
+                  sx={{ borderRadius: "50%", height: "40%" }}
+                />
+                <Image
+                  position="absolute"
+                  bottom={0}
+                  src={`images/${selfHand}.png`}
+                  alt={selfHand}
+                  objectFit="contain"
+                  sx={{ borderRadius: "50%", height: "40%" }}
+                />
+              </Centering>
+            </Box>
+          )}
+          {state === "attack/viewText" &&
+            (outcome === "win" ? (
+              <Center color="red">Attack successful!</Center>
+            ) : outcome === "lose" ? (
+              <Center color="blue">Attack failed!</Center>
+            ) : (
+              outcome === "draw" && (
+                <Center color="black">It&lsquo;s a draw</Center>
+              )
+            ))}
+        </Box>
+
+        <Field height="30%" color="red" monster={monsterSelf} isSelf={true} />
+
+        <Box ref={gtpRef} sx={{ height: "10%", width: "100%" }} pt="5px">
+          <GtpButton
+            gtpHeight={gtpHeight}
+            state={state}
+            callbackButtonSelected={handleButtonSelected}
+          />
+        </Box>
+      </Stack>
+      <Message ref={messageRef} />
+    </>
   );
 };
 
