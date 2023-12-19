187a611f0af3da73454b91ab7845a12132059402
Morisaki Mikiya <git@morisakimikiya.com>
2023-12-19 21:54:02 +0900

    update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import "@fontsource/noto-sans/400.css";
-import "@fontsource/noto-sans/600.css"; // NOTE: 700 選んでも Bold にならない？？600 で代用
-// import "@fontsource/noto-sans/700.css";
+import "@fontsource/noto-sans/600.css";
+import "@fontsource/noto-sans/700.css"; // NOTE: 700 選んでも Bold にならない？？
 import "@fontsource/noto-sans-jp/400.css";
 import "@fontsource/noto-sans-jp/600.css";
-// import "@fontsource/noto-sans-jp/700.css";
+import "@fontsource/noto-sans-jp/700.css"; // NOTE: 700 選んでも Bold にならない？？
 
 import { CssBaseline } from "@mui/material";
 import Container from "@mui/material/Container";
