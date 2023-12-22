89c146e8090bf5f0e84646825f3290fc6378cc81
Morisaki Mikiya <git@morisakimikiya.com>
2023-12-23 05:05:07 +0900

    シルエットが2つの時、3つ目のじゃんけんの手は10-90の間でランダムな値を設定する

--- backend/src/cruds/user_monster.py
+++ backend/src/cruds/user_monster.py
@@ -1,5 +1,6 @@
 """user_monsters テーブルの CRUD 関数"""
 import logging
+import random
 
 from fastapi import HTTPException
 from sqlalchemy import desc
@@ -66,7 +67,7 @@ def create_user_monster(
     if len(picture_list) == 2:
         gu = picture_list[0].match_rate
         choki = picture_list[1].match_rate
-        pa = (gu + choki) // 2
+        pa = random.randint(10, 91)  # 10 ~ 90 の間でランダムに決定
     elif len(picture_list) == 3:
         gu = picture_list[0].match_rate
         choki = picture_list[1].match_rate
